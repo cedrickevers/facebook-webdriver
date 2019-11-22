adc5c8cf78241daccbfb057e1b73d710baf2dde8
Cédric Kévers <32717814+cedrickevers@users.noreply.github.com>
2019-11-22 16:12:11 +0100

    Create navigation.php

---
+++ navigation.php
@@ -0,0 +1,21 @@
+<?php
+
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+require_once('vendor/autoload.php');
+function testNavigate(){
+    // start Chrome with 5 seconds timeout
+    
+    $host = 'http://localhost:4444/wd/hub'; // this is the default
+    $capabilities = DesiredCapabilities::firefox();
+    $driver = RemoteWebDriver::create($host, $capabilities, 5000);
+
+
+    // navigate to  
+    $driver->get('https://www.google.fr');
+        $driver->navigate()->to("https://gitlab.fusiondirectory.org/demonstration/demo-fixes");
+}
+ 
+
+testNavigate();
+
