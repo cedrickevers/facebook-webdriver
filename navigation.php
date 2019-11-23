70f548ce3d8a47cc472dd626a03d4ff43a1aa6ce
Cédric Kévers <32717814+cedrickevers@users.noreply.github.com>
2019-11-23 15:34:25 +0100

    Update navigation.php

--- navigation.php
+++ navigation.php
@@ -1,24 +1,33 @@
 <?php
 
+namespace Facebook\WebDriver;
+
 use Facebook\WebDriver\Remote\DesiredCapabilities;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
+
+
 require_once('vendor/autoload.php');
-function testNavigate(){
-    // start Chrome with 5 seconds timeout
-    
+
+function navigation() {
+ 
     $host = 'http://localhost:4444/wd/hub'; // this is the default
-    $capabilities = DesiredCapabilities::firefox();
+    $capabilities = DesiredCapabilities::chrome();
     $driver = RemoteWebDriver::create($host, $capabilities, 5000);
 
-
-    // navigate to  
-    $driver->get('https://www.google.fr');
-    $driver->navigate()->to("https://gitlab.fusiondirectory.org/demonstration/demo-fixes");
+    $driver->get("http://demo-fixes.fusiondirectory.org/fusiondirectory/");
+    $driver->navigate()->to("https://google.fr");
     $driver->navigate()->back();
     $driver->navigate()->forward();
+    $driver->navigate()->back();
+
+    $driver->findElement(WebDriverBy::id("username"))->sendKeys("test");
+    $driver->wait();
     $driver->navigate()->refresh();
+
+
 }
+
+navigation();
+
+
  
-
-testNavigate();
-
