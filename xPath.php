9fa68e3c4c7c92b864d5ac432de56953fda5241c
Cédric Kévers <32717814+cedrickevers@users.noreply.github.com>
2019-12-03 23:47:02 +0100

    Update xPath.php

--- xPath.php
+++ xPath.php
@@ -7,11 +7,9 @@ use Facebook\WebDriver\Remote\RemoteWebDriver;
 
 require_once('vendor/autoload.php');
 
-function xPath(){
+function createByxPath($driver){
 
-    $host = 'http://localhost:4444/wd/hub'; // this is the default
-    $capabilities = DesiredCapabilities::chrome();
-    $driver = RemoteWebDriver::create($host, $capabilities, 5000);
+
 // navigate to  
     $driver->get('http://demo-fixes.fusiondirectory.org/fusiondirectory/');
 
@@ -27,8 +25,24 @@ function xPath(){
     $driver->findElement(WebDriverBy::xpath('//*[@id="uid"]'))->sendKeys("usercreate");
     $driver->findElement(WebDriverBy::xpath('//*[@id="userPassword_password"]'))->sendKeys("fusion");
     $driver->findElement(WebDriverBy::xpath('//*[@id="userPassword_password2"]'))->sendKeys("fusion");
-    $driver->findElement(WebDriverBy::xpath('//*[@id="maincell"]/div/p/input[1]")'))->click();
+    $driver->findElement(WebDriverBy::xpath('//*[@id="maincell"]/div/p/input[1]'))->click();
+    $driver->wait()->until(
+        WebDriverExpectedCondition::urlIs("http://demo-fixes.fusiondirectory.org/fusiondirectory/main.php?plug=212")
+      );
+}
+function deleteUserByXPath($driver){
+    $driver->findElement(WebDriverBy::xpath('//*[@id="t_nscrollbody"]/tr[3]/td[7]/input[6]'))->click();
+    $driver->findElement(WebDriverBy::xpath('//*[@id="maincell"]/div/div/div[2]/p[4]/input[1]'))->click();
+ }
+     
+ $host = 'http://localhost:4444/wd/hub'; // this is the default
+ $capabilities = DesiredCapabilities::chrome();
+ $driver = RemoteWebDriver::create($host, $capabilities, 5000);
 
+createByxPath($driver); 
+deleteUserByXPath($driver);
+
+     
 
 
 
